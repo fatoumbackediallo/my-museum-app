fdad1f122b9dd33b0e48bcc59c7071dfeab95a7b
Fatou <fatoumbacke.diallo@gmail.com>
2023-06-29 21:55:38 +0200

    first commit

---
+++ database/FirestoreFirebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDWxxafJmSmtO19ZoLelNCxal4_SMV4F3M",
+  authDomain: "my-museum-app.firebaseapp.com",
+  projectId: "my-museum-app",
+  storageBucket: "my-museum-app.appspot.com",
+  messagingSenderId: "680363989004",
+  appId: "1:680363989004:web:a88b359dde00e969b13629",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
